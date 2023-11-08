0ba6a50c09d4f8271755ea06530ebe6ee5933d28
nguyentnam1997 <nguyentnam.061297@gmail.com>
2023-11-08 17:16:17 +0700

    123

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         //int[] nums = new int[] {0,1};
         Solution solution = new Solution();
-        int[] arr1 = new int[] {-3,-3,4,-1,-10};
-        int[] arr2 = new int[] {7,10};
-        System.out.println(solution.findTheDistanceValue(arr1, arr2, 12));
+//        int[] arr1 = new int[] {-3,-3,4,-1,-10};
+//        int[] arr2 = new int[] {7,10};
+//        System.out.println(solution.findTheDistanceValue(arr1, arr2, 12));
+        int[] nums = new int[] {4,3,6,16,8,2};
+        System.out.println(solution.longestSquareStreak(nums));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
     public int searchInsert(int[] nums, int target) {
@@ -52,5 +54,49 @@ public class Solution {
         }
         if(countNum == 0) count++;
         return findByRecursion(arr1, arr2, d, count, ++i);
+    }
+    public int longestSquareStreak(int[] nums) {
+        if (nums.length < 2) return -1;
+        Arrays.sort(nums);
+        List<Integer> numsList = new ArrayList<>();
+        for(int num: nums) {
+            numsList.add(num);
+        }
+        //numsList.addAll(Arrays.asList(nums));
+        int count = 1;  //biến lưu trữ kết quả sau mỗi lần lặp để so sánh với maxResult
+        int maxResult = 1;  //biến return kquả cao nhất
+        int i = 0;  //biến index chạy từ đầu
+        int iValue = nums[i];   //giá trị tại biến index
+        return recursion(numsList, i,iValue, count, maxResult);
+    }
+    //2 3 4 6 8 16
+    //2 3 6 9 16
+    public int recursion(List<Integer> numsList, int i, int iValue, int count, int maxResult) {
+        if (count > maxResult) {
+            maxResult = count;
+        }
+        if (i == numsList.size()) {
+            // if (count > maxResult) {
+            //     maxResult = count;
+            // }
+            if (maxResult == 1) return -1;
+            return maxResult;
+        }
+        count = 1; //sau mỗi lần đệ quy, thì count sẽ quay về 1
+        int square = (int) Math.pow(iValue, 2);     //biến tính bình phương tại index i
+        for(int j = i + 1; j < numsList.size(); ) {
+            if (numsList.get(i) == numsList.get(j)) {
+                numsList.remove(j);
+                continue;
+            }
+            if (square == numsList.get(j)) {
+                iValue = numsList.get(j);
+                numsList.remove(j);
+                count++;
+            }
+            else j++;
+        }
+        //i++;
+        return recursion(numsList, i + 1, iValue, count, maxResult);
     }
 }
